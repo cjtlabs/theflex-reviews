dacaaee3e88e678ccc607cbd03e93edb97005112
Christopher Tolang <christopher@runalloy.com>
2025-09-14 13:29:48 +0800

    fix: axios request timeout increase to accomodate server cold start

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api",
-  timeout: 10000,
+  timeout: 30000, // 30 seconds to give server ample time to cold start since we're using Render Free Tier
 });
 
 export const setAuthToken = (token: string) => {
